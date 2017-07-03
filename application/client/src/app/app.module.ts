2cc1eb01cc2d238ee16fd296602c221e3b9d1d65
Aleksandar Diklic <rastasheep3@gmail.com>
2017-07-03 22:58:11 +0200

    Most basic editor component with routes

--- application/client/src/app/app.module.ts
+++ application/client/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { EditorModule } from './editor/editor.module';
 
 import { AppComponent } from './app.component';
+
+export const ROUTES: Routes = [];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES),
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
